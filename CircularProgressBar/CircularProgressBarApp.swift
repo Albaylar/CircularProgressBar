f18e9d3baf78b8b5efa869b6a8dd4d0473bb239f
Albaylar <furkandenizalbaylar@gmail.com>
2023-09-14 13:15:10 +0300

    Initial Commit

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarApp.swift
+//  CircularProgressBar
+//
+//  Created by Furkan Deniz Albaylar on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
